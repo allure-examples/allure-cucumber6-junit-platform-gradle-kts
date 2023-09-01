c75b44d511330b0e8a3f58e63ab2424dacf1a8e4
Dmitry Baev <baev@users.noreply.github.com>
2023-09-01 16:04:55 +0100

    init commit

---
+++ src/test/java/io/qameta/allure/examples/cucumber6/CucumberTest.java
@@ -0,0 +1,11 @@
+package io.qameta.allure.examples.cucumber6;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class CucumberTest {
+}
